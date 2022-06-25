1fd9dc610dd78cfa05fd88da3e8c3e3877f1311c
nonanonno <as.nonanonno@gmail.com>
2022-06-25 17:19:02 +0000

    chore: add devcontainer

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install utilities
+packages=(
+    fzf
+    bash-completion
+)
+
+sudo apt-get update
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends "${packages[@]}"
